5dee656bf602535acc8149c4760631e7294e1ea0
unknown <kazuto.takeuchi@A0169-Win7.mita.pa-rk.co.jp>
2015-03-02 13:59:44 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+## vibe.d RESTful sample
+
+
+### Quick Start on Windows
+
+Install [dmd](http://dlang.org/download.html) and [dub](http://code.dlang.org/) via [Chocolatey](https://chocolatey.org/).
+
+```
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
+
+cinst dmd
+cinst dub
+```
+
+Start server.
+
+```
+dub
+```
+
+
+### See Also
+
+* [vibe.d](http://vibed.org/)
+* [w2ui](http://w2ui.com/)
---
+++ source/app.d
@@ -0,0 +1,58 @@
+import vibe.d;
+
+struct User {
+    int id;
+    string name;
+}
+
+interface IUserApi {
+    @path("")     User[] get();
+    @path(":id")  User   get(int _id);
+    @path("")     void   add(int id_, string name);
+    @path(":id")  void   put(int _id, string name);
+    @path(":id")  void   remove(int _id);
+}
+
+class UserApi : IUserApi {
+    private User[int] _users;
+
+    this(User[] users) {
+        foreach (user; users) {
+            _users[user.id] = user;
+        }
+    }
+
+    User[] get() {
+        return _users.values;
+    }
+
+    User get(int id) {
+        return *enforceHTTP(id in _users, HTTPStatus.notFound);
+    }
+
+    void add(int id, string name) {
+        enforceHTTP(id !in _users, HTTPStatus.conflict);
+        _users[id] = User(id, name);
+    }
+
+    void remove(int id) {
+        _users.remove(id);
+    }
+
+    void put(int id, string name) {
+        enforceHTTP(id in _users, HTTPStatus.notFound).name = name;
+    }
+}
+
+shared static this() {
+    auto router = new URLRouter();
+    router.get("/", (req, res) => res.render!("index.dt", req));
+    router.registerRestInterface(new UserApi([User(1, "Alice"), User(2, "Bob")]), "users");
+
+    auto settings = new HTTPServerSettings;
+    settings.port = 8080;
+    settings.bindAddresses = ["::1", "127.0.0.1"];
+    settings.listenHTTP(router);
+
+    logInfo("Please open http://localhost:8080/ in your browser.");
+}
