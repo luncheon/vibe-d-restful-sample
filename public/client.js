30b1158e335deb48657b3a59bd79ef0b01f1f89a
unknown <kazuto.takeuchi@A0169-Win7.mita.pa-rk.co.jp>
2015-03-18 17:09:13 +0900

    Avoid using Array.prototype.findIndex (unsupported by IE 11 yet).

--- public/client.js
+++ public/client.js
@@ -1,127 +1,126 @@
 var client = (function () {
     'use strict';
 
-    this.RestClient = (function () {
-        var request = function (url, method, data) {
-            return $.ajax({
-                url:         url,
-                type:        method,
-                data:        data ? JSON.stringify(data) : '',
-                contentType: 'application/json',
-                dataType:    'json',
-                cache:       false,
-            }).fail(function (xhr, status, thrown) {
-                console.error({xhr: xhr, status: status, thrown: thrown}, xhr.responseText);
-                alert(thrown);
-            });
+    var request = function (url, method, data) {
+        return $.ajax({
+            url:         url,
+            type:        method,
+            data:        data ? JSON.stringify(data) : '',
+            contentType: 'application/json',
+            dataType:    'json',
+            cache:       false,
+        }).fail(function (xhr, status, thrown) {
+            console.error({xhr: xhr, status: status, thrown: thrown}, xhr.responseText);
+            alert(thrown);
+        });
+    }
+
+    this.RestClient = function (baseurl) {
+        this.get    = function ()         { return request(baseurl,            'GET'); }
+        this.delete = function (id)       { return request(baseurl + '/' + id, 'DELETE'); }
+        this.create = function (data)     { return request(baseurl,            'POST', data); }
+        this.update = function (id, data) { return request(baseurl + '/' + id, 'PUT', data); }
+
+        this.login  = function (data) {
+            return $.post('login', data).done(function (data, status, xhr) {
+                var cookie = xhr.getResponseHeader('Set-Cookie');
+                if (cookie) {
+                    document.cookie = cookie;
+                }
+                location.reload();
+            }).fail(function () { alert('login failure'); });
         }
-
-        return function (baseurl) {
-            this.get    = function ()         { return request(baseurl,            'GET'); }
-            this.delete = function (id)       { return request(baseurl + '/' + id, 'DELETE'); }
-            this.create = function (data)     { return request(baseurl,            'POST', data); }
-            this.update = function (id, data) { return request(baseurl + '/' + id, 'PUT', data); }
-
-            this.login  = function (data) {
-                return $.post('login', data).done(function (data, status, xhr) {
-                    var cookie = xhr.getResponseHeader('Set-Cookie');
-                    if (cookie) {
-                        document.cookie = cookie;
-                    }
-                    location.reload();
-                }).fail(function () { alert('login failure'); });
-            }
-            this.logout = function () {
-                return $.post('logout').then(function () { location.reload(); });
-            }
-        };
-    })();
-
-    this.LocalStorage = (function () {
-        var byId = function (id) {
-            return function (item) { return item.id === id; }
+        this.logout = function () {
+            return $.post('logout').then(function () { location.reload(); });
         }
-        var resolve = function (data) {
-            var deferred = new $.Deferred;
-            deferred.resolve(data);
-            return deferred.promise();
-        }
-        var reject = function (data) {
-            var deferred = new $.Deferred;
-            deferred.reject(data);
-            alert(data);
-            return deferred.promise();
-        }
+    }
 
-        return function (baseurl) {
-            this.load = function () {
-                return JSON.parse(localStorage.getItem(baseurl)) || [];
+    var find = function (array, id) {
+        for (var i = 0, len = array.length; i < len; i++) {
+            if (array[i].id === id) {
+                return array[i];
             }
-            this.save = function (items) {
-                localStorage.setItem(baseurl, JSON.stringify(items));
-            }
-            this.get = function (id) {
-                if (id) {
-                    var found = this.load().find(byId(id));
-                    return found ? resolve(found) : reject('Not Found');
-                } else {
-                    return resolve(this.load());
-                }
-            }
-            this.delete = function (id) {
-                this.save(this.load().filter(function (item) { return item.id !== id; }));
-                return resolve();
-            }
-            this.create = function (data) {
-                if (!data || !data.id || !data.name) {
-                    return reject('Bad Request');
-                }
-                var items = this.load();
-                if (items.some(byId(data.id))) {
-                    return reject('Conflict');
-                }
-                items.push(data);
-                this.save(items);
-                return resolve();
-            }
-            this.update = function (id, data) {
-                var items = this.load();
-                var index = items.findIndex(byId(id));
-                if (index === -1) {
-                    return reject('Not Found');
-                }
-                if (data.id && data.id !== id && items.some(byId(data.id))) {
-                    return reject('Conflict');
-                }
-                for (var key in items[index]) {
-                    items[index][key] = data[key] || items[index][key];
-                }
-                this.save(items);
-                return resolve();
-            }
-            this.getSessionUser = function () {
-                return JSON.parse(sessionStorage.getItem('user'));
-            }
-            this.login = function (data) {
-                var users = this.load();
-                var index = users.findIndex(function (user) { return user.id === data.id; });
-                var user = index === -1 ? null : users[index];
+        }
+        return null;
+    }
 
-                if (user && user.password === data.password) {
-                    sessionStorage.setItem('user', JSON.stringify(user));
-                    location.reload();
-                    return resolve();
-                } else {
-                    return reject('login failure');
-                }
+    var resolve = function (data) {
+        var deferred = new $.Deferred;
+        deferred.resolve(data);
+        return deferred.promise();
+    }
+
+    var reject = function (data) {
+        var deferred = new $.Deferred;
+        deferred.reject(data);
+        alert(data);
+        return deferred.promise();
+    }
+
+    this.LocalStorage = function (baseurl) {
+        this.load = function () {
+            return JSON.parse(localStorage.getItem(baseurl)) || [];
+        }
+        this.save = function (items) {
+            localStorage.setItem(baseurl, JSON.stringify(items));
+        }
+        this.get = function (id) {
+            if (id) {
+                var found = find(this.load(), id);
+                return found ? resolve(found) : reject('Not Found');
             }
-            this.logout = function () {
-                sessionStorage.clear();
+            return resolve(this.load());
+        }
+        this.delete = function (id) {
+            this.save(this.load().filter(function (item) { return item.id !== id; }));
+            return resolve();
+        }
+        this.create = function (data) {
+            if (!data || !data.id || !data.name) {
+                return reject('Bad Request');
+            }
+            var items = this.load();
+            if (find(items, data.id)) {
+                return reject('Conflict');
+            }
+            items.push(data);
+            this.save(items);
+            return resolve();
+        }
+        this.update = function (id, data) {
+            var items = this.load();
+            var item = find(items, id);
+            if (!item) {
+                return reject('Not Found');
+            }
+            if (data.id && data.id !== id && find(items, data.id)) {
+                return reject('Conflict');
+            }
+            for (var key in item) {
+                item[key] = data[key] || item[key];
+            }
+            this.save(items);
+            return resolve();
+        }
+        this.getSessionUser = function () {
+            return JSON.parse(sessionStorage.getItem('user'));
+        }
+        this.login = function (data) {
+            var user = find(this.load(), data.id);
+            if (user && user.password === data.password) {
+                sessionStorage.setItem('user', JSON.stringify(user));
                 location.reload();
                 return resolve();
+            } else {
+                return reject('login failure');
             }
         }
-    })();
+        this.logout = function () {
+            sessionStorage.clear();
+            location.reload();
+            return resolve();
+        }
+    }
 
     return this;
 }).apply(this.client || {});
